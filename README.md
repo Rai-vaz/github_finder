eace5bf1dde03f82cf144f81b1d24e8de606c2e2
Rai-vaz <ray.vaz04@gmail.com>
2023-02-23 08:39:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# github_finder
+ Aplicação que busca usuário do github e mostra seu perfil em layout customizado usando typeScript
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import classes from "./css/App.module.css"
+function App() {
+
+  return (
+    <div className={classes.app}>
+      <h1>Github finder</h1>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default App
